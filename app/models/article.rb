e5af702e7589bb7fcb160992e6dd687267dfc054
Alexey Osipenko <aratak@anahoret.com>
2011-11-14 17:44:47 +0200

    add validation to model user and article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ActiveRecord::Base
   # belongs_to :user, :foreign_key => "author_id"
   belongs_to :author, :class_name => "User", :foreign_key => "author_id"
   default_scope :order => "#{self.quoted_table_name}.created_at DESC"
+  
+  validates :title,  :presence => true,
+                     :length   => { :maximum => 150 }
+  validates :content, :presence => true
+                    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
   has_many :articles, :foreign_key => "author_id"  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+                      
 end
